46598511e918440898bcf3684947aaea06208a75
ShotaOgino <ogishou.shouta@gmail.com>
2023-01-01 11:27:17 +0900

    Galleryへの画像の追加
    Galleryへの画像の追加を行いました。
    あと、ページネーションの位置が必要です。

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,29 @@
 let vw = 0.04266666667 * window.innerWidth;
 var mySwiper = new Swiper('.swiper-container', {
-	centeredSlides: true,
-	loop: true,
+    centeredSlides: true,
+    loop: true,
+    autoHeight: true,
     spaceBetween: 10,
     centeredSlides: true,
     slidesPerView: 'auto',
-	breakpoints: {
-		767: {
-			spaceBetween: vw,
-		  }
-	},
-	autoplay: {
-		delay: 3000,
-		stopOnLastSlide: false,
-		disableOnInteraction: false,
-		reverseDirection: false
-	},
+    breakpoints: {
+        767: {
+            spaceBetween: vw,
+        }
+    },
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+        reverseDirection: false
+    },
     navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
     pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
-	}
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    }
 });
